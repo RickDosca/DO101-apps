106e7788f6cbbcd9ece1295d898f58ce822220c4
RickDosca <32435166+RickDosca@users.noreply.github.com>
2021-05-30 20:20:03 -0300

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hola Mundo!\n');
+  res.send('Prueba de concepto!\n');
 });
 
 app.listen(8080, function () {
