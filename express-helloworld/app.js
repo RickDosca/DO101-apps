9240cbee845927494ae2ef92b10e984e70c758b7
Rick <rick.dosca@gmail.com>
2021-05-30 18:16:38 -0300

    Cambio a HOLA MUNDO

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hola Mundo!\n');
 });
 
 app.listen(8080, function () {
